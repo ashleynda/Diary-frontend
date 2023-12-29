8432950a2bf222d287ed6ecbdf70ddb1d905ab99
Ashley <ashleyndabai@gmail.com>
2023-12-29 16:17:26 +0100

    update

--- dashboard.js
+++ dashboard.js
@@ -2,17 +2,17 @@ let newEntry = document.querySelector('#create-entry-part');
 newEntry.addEventListener('submit', () => {
     event.preventDefault();
 
-    let userName = localStorage.getItem('Username');
-    let title = document.getElementById('title-entry').value
-    let body = document.getElementById('body-entry').value
+    clearpane();
+
+    userName = localStorage.getItem('Username');
+    title = document.getElementById('title-entry').value
+    body = document.getElementById('body-entry').value
 
     let newEntryRequest = {
-        userId: userName,
+        userName: userName,
         title: title,
         body: body
     };
-
-    console.log(newEntryRequest);
     const newEntryUrl = 'http://localhost:8080/Journal/createNewEntry';
 
     fetch(newEntryUrl, {
@@ -22,52 +22,87 @@ newEntry.addEventListener('submit', () => {
             'Content-Type': 'application/json; charset=UTF-8'
         },
     })
+    
     .then(response => response.json())
     .then(responseObject => {
+        console.log(responseObject);
         if(typeof responseObject.data !== 'string'){
             document.getElementById("create-entry-text").innerHTML = "Entry created successfully";
-            // document.getElementById("create-entry-text").innerHTML = responseObject.data.message;
-            // window.location = './dashboard.html';
         } else {
             let response = document.getElementById("create-entry-text");
             response.innerHTML = responseObject.data;
             response.style.color = 'red';
         }        
     })
+  
     .catch(error => {
         console.error('Error:', error)
     })
 });
 
+let updateEntry = document.querySelector("#update-entry-part");
+updateEntry.addEventListener('submit', () => {
+    event.preventDefault();
+
+    let userName = localStorage.getItem('Username');
+    let title = document.getElementById('update-title').value;
+    let body = document.getElementById('update-body').value;
+
+    let updateEntryRequest = {
+        userName: userName,
+        title: title,
+        body: body
+    };
+
+    const updateEntryUrl = 'http://localhost:8080/Journal/updateEntry';
+    fetch(updateEntryUrl, {
+        method: 'PATCH',
+        body: JSON.stringify(updateEntryRequest),
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8'
+        },
+    })
+    .then(response => response.json())
+    .then(responseObject => {
+        console.log(responseObject)
+        if(typeof responseObject.data !== 'string'){
+            document.getElementById("update-entry-text").innerText = "responseObject.data.message";
+        } else {
+            let response = document.getElementById("update-entry-text");
+            response.innerHTML = responseObject.data;
+            response.style.color = 'red';
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    })    
+});
+
 let findEntry = document.querySelector("#find-entry-part");
 findEntry.addEventListener('submit', () => {
     event.preventDefault();
+    clearpane();
    
-    // document.getElementById('find-entry-part').style.display='flex';
-    // clearpane()
-    username = localStorage.getItem('Username');
+    userName = localStorage.getItem('Username');
     title = document.getElementById('find-title-entry').value;
 
     let findEntryRequest = {
-        usernameId: username,
         title: title,
     };
 
-    console.log(findEntryRequest);
-    const findEntryUrl = 'http://localhost:8080/Journal/findEntry/{userName}/{title}';
+    const findEntryUrl = `http://localhost:8080/Journal/findEntry/${userName}/${title}`;
 
     fetch(findEntryUrl, {
         method: 'GET',
-        // body: JSON.stringify(findEntryRequest),
         headers: {
             'Content-Type': 'application/json; charset=UTF-8',
         },
     })
     .then(response => response.json())
     .then(responseObject => {
-        // document.getElementById("find-entry-text").innerText = responseObject.data.message;
+        console.log(responseObject)
         if(typeof responseObject.data !== 'string'){
-            document.getElementById("find-entry-text").innerText = responseObject.data.message;
+            document.getElementById("find-entry-text").innerText = "Entry found successfully";
         } else {
             let response = document.getElementById("find-entry-text");
             response.innerHTML = responseObject.data;
@@ -82,14 +117,17 @@ findEntry.addEventListener('submit', () => {
 let deleteEntry = document.querySelector("#delete-entry-part");
 deleteEntry.addEventListener('submit', () => {
     event.preventDefault();
+    clearpane();
 
-    deleteValue = document.getElementById('delete-entry-diary').value;
+
+    userName = localStorage.getItem('Username');
+    title = document.getElementById('title-entry').value;
 
     let deleteEntryRequest = {
-        delete: deleteValue
+        userName: userName,
+        title: title
     };
 
-    console.log(deleteEntryRequest);
     const deleteEntryUrl = 'http://localhost:8080/Journal/delete';
 
     fetch(deleteEntryUrl, {
@@ -101,38 +139,36 @@ deleteEntry.addEventListener('submit', () => {
     })
     .then(response => response.json())
     .then(responseObject => {
-        document.getElementById("delete-entry-text").innerText = responseObject.data;          
-    })
+        console.log(responseObject);        
+        if(typeof responseObject.data !== 'string'){
+            document.getElementById("delete-entry-text").innerHTML = "Deleted successfully";
+        } else {
+            let response = document.getElementById("delete-entry-text");
+            response.innerHTML = responseObject.data;
+            response.style.color = 'red';
+        } 
+    })       
     .catch(error => {
         console.error('Error:', error)
     })  
 });
 
-
-
-
-
-
 let newEntryForm = document.querySelector('#createNewEntry-button');
 newEntryForm.addEventListener('click', () => {
     clearpane();
-    document.getElementById('create-entry-part').style.display='flex';
-    // document.getElementById('create-entry-new').style.display='flex';
-   
+    document.getElementById('create-entry-part').style.display='flex';  
 });
 
-// let newEntryForm = document.querySelector('#createNewEntry-button');
-// newEntry.addEventListener('click', () {
-//  
-//     document.getElementById('create-entry-new').style.display='flex';
-// });
+let updateEntryForm = document.querySelector('#updateEntry-button');
+updateEntryForm.addEventListener('click', () => {
+    clearpane(); 
+    document.getElementById('update-entry-part').style.display='flex';
+});
 
 let findEntryForm = document.querySelector('#findEntry-button');
 findEntryForm.addEventListener('click', () => {
     clearpane();
     document.getElementById('find-entry-part').style.display='flex';    
-    // document.getElementById('find-entry').style.display='flex';
-    
 });
 
 let deleteEntryForm = document.querySelector('#deleteEntry-button');
@@ -144,12 +180,10 @@ deleteEntryForm.addEventListener('click', () => {
 
 function clearpane() {
     document.getElementById('create-entry-part').style.display='none';
+    document.getElementById('update-entry-part').style.display='none';
     document.getElementById('find-entry-part').style.display='none';
-    document.getElementById('delete-entry-part').style.display='none';
-    
-}
-
-
+    document.getElementById('delete-entry-part').style.display='none';    
+};
 
 let logout = document.getElementById('logout');
 logout.addEventListener('click', () => {
